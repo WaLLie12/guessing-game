c7818daf01c6a639cfdcb910aa265b949ad40725
vragno01 <vadimserapionov@gmail.com>
2023-10-06 20:28:55 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minRange = 0;
+        this.maxRange = 0;
+        this.currentGuess = 0;
+    }
 
     setRange(min, max) {
-
+        this.minRange = min;
+        this.maxRange = max;
     }
 
     guess() {
-
+        this.currentGuess = Math.floor((this.minRange + this.maxRange) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.maxRange = this.currentGuess;
     }
 
     greater() {
-
+        this.minRange = this.currentGuess + 1;
     }
 }
+
 
 module.exports = GuessingGame;
